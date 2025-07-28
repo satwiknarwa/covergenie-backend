0db47f493cd71c7a2fae4245fd5af0b410ced8e1
acg-sn1005 <sn1005@aagama.org>
2025-07-28 16:12:05 +0530

    settings some commands added

--- covergenie_backend/settings.py
+++ covergenie_backend/settings.py
@@ -14,7 +14,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = ['covergenie-backend.onrender.com']
+ALLOWED_HOSTS = [   'covergenie-backend-eoeg.onrender.com',
+    'localhost',
+    '127.0.0.1'
+]
 
 # Application definition
 INSTALLED_APPS = [
